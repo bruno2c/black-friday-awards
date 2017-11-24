bb3458028de9163ca9eb86792d42a0253f19e584
Dhiego Vieira <vieira.dhiego@gmail.com>
2017-11-24 10:08:52 -0200

    [BlackFriday] winners

---
+++ web/js/gallery/examples/src/appWinners.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import fetch from 'isomorphic-fetch'
+import Winners from './winner';
+
+function getWinners() {
+    return async function () {
+        const url = `http://bfawards.local/contest/${this.state.contestId}/winners`;
+        try {
+            const response = await fetch(url, {
+                credentials: 'same-origin'
+            });
+            const json = await response.json();
+
+            this.setState({winners: json.ranking});
+
+        } catch (e) {
+            console.log('error', e)
+        }
+    }
+}
+
+class AppWinners extends React.Component {
+    constructor() {
+        super();
+        this.state = {width: -1, winners: ''};
+        this.getWinners = getWinners();
+    }
+
+    componentDidMount() {
+        this.getWinners();
+    }
+
+
+    render() {
+        return (
+                <Winners winners={this.state.winners}/>
+        )
+    }
+}
+
+ReactDOM.render(<AppWinners/>, document.getElementById('app'));
